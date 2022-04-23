acd30f9c60c1019e7be4ec2cb9da557aba686ebf
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-04-23 06:40:18 +0530

    just added some more

--- A/1343A.cpp
+++ A/1343A.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
-    int n,m;
-    cin >> m;
-    for (int i = 0 ; i < m ; i++){
-        cin >> n;
-        for (int k = 2 ; k <= 29 ; k++) {   
-            int x = pow(2,k);   
-            if (n%(x-1) == 0)
-                cout << i << ' ' << n << endl;   
-                cout << n/(x-1) << '\n';
-        }
-    }
-    return 0;
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int pw = 2; pw < 30; ++pw) {
+			int val = (1 << pw) - 1;
+			if (n % val == 0) {
+				// cerr << val << endl;
+				cout << n / val << endl;
+				break;
+			}
+		}
+	}
+	return 0;
 }
